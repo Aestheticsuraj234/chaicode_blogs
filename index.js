07e2e88763a1f69993b54da99d9bf0541cb513bf
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-01-07 15:37:36 +0530

    02 new branch

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');  
 });
 const PORT = process.env.PORT || 3000;
+
+app.use('/api', (await import('./routes/user.routes.js')).default);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
